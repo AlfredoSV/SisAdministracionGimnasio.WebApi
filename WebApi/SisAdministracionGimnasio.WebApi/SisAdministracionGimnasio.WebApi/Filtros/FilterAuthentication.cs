393aa189e51b095da16242941dd1137b637c0a4f
Alfredo Sánchez <alfredosanchezverduzco@outlook.com>
2022-03-19 07:03:01 -0600

    Se agregan clases FilterAuthentication y FilterAuthorize

---
+++ WebApi/SisAdministracionGimnasio.WebApi/SisAdministracionGimnasio.WebApi/Filtros/FilterAuthentication.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SisAdministracionGimnasio.WebApi.Filtros
+{
+    public class FilterAuthentication : IActionFilter
+    {
+        public  void OnActionExecuted(ActionExecutedContext context)
+        {
+            
+        }
+
+        public  void OnActionExecuting(ActionExecutingContext context)
+        {
+           
+            
+        }
+    }
+}
